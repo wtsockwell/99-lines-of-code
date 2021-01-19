d18207ac6a1a0773965ea67ba9bf9467e68be2a5
Taylor Sockwell <wtsockwell@yahoo.com>
2021-01-19 14:42:37 -0600

    Copied 99 lines of code project and added a loop for naming friends, as well as cleaning the code up a bit

---
+++ app.js
@@ -0,0 +1,21 @@
+friends = ["Thomas Anderson", "Morpheus", "Smith", "Trinity", "Dozer"]
+
+for (let voice = 0; voice < friends.length; voice++) {
+    code(friends[voice]);
+}
+
+function code(name) {
+    let lyrics = [" lines of code", " in the file,", " strikes one out, clears it all out", " line of code", " strikes it out, clears it all out", " no more lines of code in the file"];
+    for (let i = 99; i > 0; i --){
+        if (i>3) {
+            console.log(i + lyrics[0] + lyrics[1] + " " + i + lyrics[0] + "; " + name + lyrics[2]);
+            console.log(i -1 + lyrics[0] + lyrics[1]);
+        } else if (i == 2) {
+            console.log(i + lyrics[0] + lyrics[1] + " " + i + lyrics[0] + "; " + name + lyrics[2]);
+            console.log(i -1 + lyrics[3] + lyrics[1]);
+        } else {
+            console.log(i + lyrics[3] + lyrics[1] + " " + i + lyrics[3] + "; " + name + lyrics[4]);
+            console.log(lyrics[5]);
+        }
+    }
+}
